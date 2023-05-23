fee24f5aa7f0d7fbfbbfa81fd2fb0590ff63f829
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-05-24 00:35:48 +0300

    added image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,22 @@
-// import Navbar from "./Header";
+import React from 'react';
 import './App.css';
 
-
 function Home() {
-
-    return (
-      <div className="home" id="homeComp" >
-      <h1 name='home'>Hi, I am Asta <br /> Front-end Web Developer </h1>
-      <p id="statement">You landed on my portfolio website. I post my projects here as I learn.</p>
+  return (
+    <div className="home" id="homeComp">
+      <div className="row">
+        <div className="col-md-4">
+          <img id="profile" src={process.env.PUBLIC_URL + "/profilePhoto.jpg"} alt="Profile" />
+        </div>
+        <div className="col-md-8">
+          <div className="ms-3">
+            <h1 name='home'>Hi, I am Asta <br /> Front-end Web Developer</h1>
+            <p id="statement">You landed on my portfolio website. I post my projects here as I learn.</p>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Home;
-  
