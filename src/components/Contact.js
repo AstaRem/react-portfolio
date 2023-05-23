1701f3863795c40ce9bc41c657c154ae97483459
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-05-24 00:06:20 +0300

    projects, contacts done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,36 +2,26 @@
 
 function Contact(){
     return(
+    <>
     <form>
         {/* <h2 name="contact">Here will be the form to contact me</h2> */}
         <form>
-  <div  name="contact" className="form-group">
-    <label class="form-input" for="exampleFormControlInput1">Your Email address:</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-  </div>
-  <div className="form-group">
-    <label class="form-input" for="exampleFormControlTextarea1">Your message:</label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>
-{/* Must include contact information:
-
-Email address
-
-Link to a PDF version of your CV
-
-Link to your GitHub profile
-
-Link to your LinkedIn page
-
-Must have a contact form for handling events */}
-
-<p className="contacts">My Email: asta.rem777@gmail.com</p>
-<p className="contacts">You can download a PDF version of my CV <a href="#">here</a>.</p>
-<p className="contacts">Here is my <a href="#">GitHub</a>.</p>
-<p className="contacts">You can find me on <a href="#">LinkedIn</a>.</p>
-
+          <div  name="contact" className="form-group">
+            <label class="form-input" for="exampleFormControlInput1">Your Email address:</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <div className="form-group">
+            <label class="form-input" for="exampleFormControlTextarea1">Your message:</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
     </form>
+    </form>
+      <p className="contacts">My Email: asta.rem777@gmail.com</p>
+      <p className="contacts">You can download a PDF version of my CV <a href={process.env.PUBLIC_URL + "/CV.pdf"} >here</a>.</p>
+      <p className="contacts">Here is my <a href="https://github.com/AstaRem">GitHub</a>.</p>
+      <p className="contacts">You can find me on <a href="https://www.linkedin.com/in/asta-rem-developer/">LinkedIn</a>.</p>
+</>
+    
     )
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,26 +2,24 @@
 function Project({data}){
     console.log(data.image);
     return(
-        <div className="container">
-  {/* <div class="row"> */}
-    {/* <div class="col-sm-2"> */}
-    
-
-
-        <div className="card">
-            <img src={data.image} className="card-img-top" alt="" />
+<div className="container">
+      <div className="card">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + data.image} className="card-img" alt="..." />
+          </div>
+          <div className="col-md-8">
             <div className="card-body">
-                <h5 className="card-title">{data.title}</h5>
-                {/* <p className="card-text">{data.description}</p> */}
-                <a href={data.linkGitH} className="btn btn-secondary">GitHub repository</a>
+              <h5 className="card-title">{data.title}</h5>
+              <div className="d-flex justify-content-end">
+                <a href={data.linkGitH} className="btn btn-secondary me-2">GitHub repository</a>
                 <a href={data.linkDepl} className="btn btn-secondary">Deployed site</a>
+              </div>
             </div>
+          </div>
         </div>
-
-        {/* </div>
-        </div> */}
-        </div>
-    )
+      </div>
+    </div>    )
 }
 
 export default Project;
