fdbf3ce6f4dcfb9dae9f172e5bc7f14419be2bc1
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-06-23 10:49:51 +0100

    successfully moved Navbar to it's own component, navigation is working properly

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
 import './components/App.css';
-import Header from './components/Header'
-import Home from './components/Home';
-import Contact from './components/Contact';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+// import Home from './components/Home';
+// import Contact from './components/Contact';
 import Footer from './components/Footer';
-import ProjectGallery from './components/ProjectGallery';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import ProjectGallery from './components/ProjectGallery';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Header />
-    <Router>
-      
-      <nav class="navigation" >
-        <ul className="nav justify-content-start">
-          <li className="nav-item">
-            <Link  className="nav-link" to="/react-portfolio">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/react-portfolio/project-gallery">Projects</Link>
-          </li>
-
-          <li className="nav-item">
-            <Link className="nav-link" to="/react-portfolio/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* <li className={`nav-item ${activeNavItem === 'home' ? 'active' : ''}`}>
-  <Link className="nav-link" to="/react-portfolio" onClick={() => setActiveNavItem('home')}>
-    Home
-  </Link>
-</li>
-
-<li className={`nav-item ${activeNavItem === 'projects' ? 'active' : ''}`}>
-  <Link className="nav-link" to="/react-portfolio/project-gallery" onClick={() => setActiveNavItem('projects')}>
-    Projects
-  </Link>
-</li>
-
-<li className={`nav-item ${activeNavItem === 'contact' ? 'active' : ''}`}>
-  <Link className="nav-link" to="/react-portfolio/contact" onClick={() => setActiveNavItem('contact')}>
-    Contact
-  </Link>
-</li>
- */}
-
-
-      <Routes>
-        <Route path="/react-portfolio" element={<Home />} />
-        <Route path="/react-portfolio/project-gallery" element={<ProjectGallery />} />      
-        <Route path="/react-portfolio/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-
+    <Navbar />
     <Footer />
 
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,58 @@
+import Home from './Home';
+import Contact from './Contact';
+import ProjectGallery from './ProjectGallery';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-// function Navbar(){
-//     return (
-// <ul className="nav justify-content-end">
-//   <li className="nav-item">
-//     <a className="nav-link active" href="./src/components/Home">Home</a>
-//   </li>
-//   <li className="nav-item">
-//     <a className="nav-link" href="./src/components/ProjectGallery">Projects</a>
-//   </li>
-//   <li className="nav-item">
-//     <a className="nav-link" href="#">Contact</a>
-//   </li>
-// </ul>    );
-// }
+
+
+function Navbar(){
+    return (
+        <Router>
+      
+        <nav class="navigation" >
+          <ul className="nav justify-content-start">
+            <li className="nav-item">
+              <Link  className="nav-link" to="/react-portfolio">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/react-portfolio/project-gallery">Projects</Link>
+            </li>
+  
+            <li className="nav-item">
+              <Link className="nav-link" to="/react-portfolio/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+  
+        {/* <li className={`nav-item ${activeNavItem === 'home' ? 'active' : ''}`}>
+    <Link className="nav-link" to="/react-portfolio" onClick={() => setActiveNavItem('home')}>
+      Home
+    </Link>
+  </li>
+  
+  <li className={`nav-item ${activeNavItem === 'projects' ? 'active' : ''}`}>
+    <Link className="nav-link" to="/react-portfolio/project-gallery" onClick={() => setActiveNavItem('projects')}>
+      Projects
+    </Link>
+  </li>
+  
+  <li className={`nav-item ${activeNavItem === 'contact' ? 'active' : ''}`}>
+    <Link className="nav-link" to="/react-portfolio/contact" onClick={() => setActiveNavItem('contact')}>
+      Contact
+    </Link>
+  </li>
+   */}
   
   
-// export default Navbar;
+        <Routes>
+          <Route path="/react-portfolio" element={<Home />} />
+          <Route path="/react-portfolio/project-gallery" element={<ProjectGallery />} />      
+          <Route path="/react-portfolio/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+  
+    )
+}
+  
+  
+export default Navbar;
